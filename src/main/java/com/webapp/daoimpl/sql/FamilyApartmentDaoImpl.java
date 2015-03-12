6711f8caf581c4d86ecfd1a157bf16512c4102d8
maxlpy <maxlipengyu@gmail.com>
2015-03-12 17:12:03 -0400

    add models DaoImpl

---
+++ src/main/java/com/webapp/daoimpl/sql/FamilyApartmentDaoImpl.java
@@ -0,0 +1,11 @@
+package com.webapp.daoimpl.sql;
+
+import org.springframework.stereotype.Component;
+
+import com.webapp.dao.FamilyApartmentDao;
+import com.webapp.model.FamilyApartment;
+
+@Component
+public class FamilyApartmentDaoImpl extends BaseSQLImpl<FamilyApartment> implements FamilyApartmentDao {
+
+}
